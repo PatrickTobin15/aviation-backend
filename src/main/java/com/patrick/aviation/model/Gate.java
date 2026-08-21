e4f5cefcfc44949d8a65ff36e49e5ef04027626f
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 02:21:38 -0230

    Update Gate.java

--- src/main/java/com/patrick/aviation/model/Gate.java
+++ src/main/java/com/patrick/aviation/model/Gate.java
@@ -1,1 +1,31 @@
+package com.patrick.aviation.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "gates")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Gate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // example "B12"
+    @NotBlank
+    private String gateNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "airport_id", nullable = false)
+    @JsonIgnore
+    private Airport airport;
+}

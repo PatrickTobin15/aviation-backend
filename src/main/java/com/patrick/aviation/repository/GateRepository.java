f8c73a88d31dd3c2cabcfcbd8be2d39a18315d46
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 02:22:45 -0230

    Update GateRepository.java

--- src/main/java/com/patrick/aviation/repository/GateRepository.java
+++ src/main/java/com/patrick/aviation/repository/GateRepository.java
@@ -1,1 +1,10 @@
+package com.patrick.aviation.repository;
 
+import com.patrick.aviation.model.Gate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GateRepository extends JpaRepository<Gate, Long> {
+    List<Gate> findByAirportId(Long airportId);
+}

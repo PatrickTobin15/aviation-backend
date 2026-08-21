0c12081979e259bffc8c784172eabdc8bde722ec
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 02:21:53 -0230

    Update AirlineRepository.java

--- src/main/java/com/patrick/aviation/repository/AirlineRepository.java
+++ src/main/java/com/patrick/aviation/repository/AirlineRepository.java
@@ -1,1 +1,8 @@
+package com.patrick.aviation.repository;
 
+import com.patrick.aviation.model.Airline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AirlineRepository extends JpaRepository<Airline, Long> {
+    Airline findByIataCode(String iataCode);
+}

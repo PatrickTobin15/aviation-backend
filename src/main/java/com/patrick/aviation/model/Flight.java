7f768f2f3086ff94142d1c816e62bb56a58474b2
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 02:21:10 -0230

    Update Flight.java

--- src/main/java/com/patrick/aviation/model/Flight.java
+++ src/main/java/com/patrick/aviation/model/Flight.java
@@ -1,1 +1,56 @@
+package com.patrick.aviation.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "flights")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // example "AC 456"
+    @NotBlank
+    private String flightNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "airline_id", nullable = false)
+    @NotNull
+    private Airline airline;
+
+    @ManyToOne
+    @JoinColumn(name = "origin_airport_id", nullable = false)
+    @NotNull
+    private Airport originAirport;
+
+    @ManyToOne
+    @JoinColumn(name = "destination_airport_id", nullable = false)
+    @NotNull
+    private Airport destinationAirport;
+
+    @ManyToOne
+    @JoinColumn(name = "gate_id")
+    private Gate gate;
+
+    @NotNull
+    private LocalDateTime scheduledTime;
+
+    private LocalDateTime actualTime;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private FlightStatus status;
+}

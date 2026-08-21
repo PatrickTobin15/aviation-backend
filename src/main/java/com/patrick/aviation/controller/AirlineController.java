88279480eae6e86a2a24af792a964a8db90528e3
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 02:19:28 -0230

    Update AirlineController.java

--- src/main/java/com/patrick/aviation/controller/AirlineController.java
+++ src/main/java/com/patrick/aviation/controller/AirlineController.java
@@ -1,1 +1,50 @@
+package com.patrick.aviation.controller;
 
+import com.patrick.aviation.model.Airline;
+import com.patrick.aviation.service.AirlineService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/airlines")
+public class AirlineController {
+
+    private final AirlineService airlineService;
+
+    @Autowired
+    public AirlineController(AirlineService airlineService) {
+        this.airlineService = airlineService;
+    }
+
+    @GetMapping
+    public List<Airline> getAllAirlines() {
+        return airlineService.getAllAirlines();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Airline> getAirlineById(@PathVariable Long id) {
+        return ResponseEntity.ok(airlineService.getAirlineById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Airline> createAirline(@Valid @RequestBody Airline airline) {
+        Airline created = airlineService.createAirline(airline);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Airline> updateAirline(@PathVariable Long id, @Valid @RequestBody Airline airline) {
+        return ResponseEntity.ok(airlineService.updateAirline(id, airline));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAirline(@PathVariable Long id) {
+        airlineService.deleteAirline(id);
+        return ResponseEntity.noContent().build();
+    }
+}

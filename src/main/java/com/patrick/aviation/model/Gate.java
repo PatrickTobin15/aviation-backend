0267df5f332957d8434a2c29ac15b30045cf7947
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 03:20:37 -0230

    Update Gate.java

--- src/main/java/com/patrick/aviation/model/Gate.java
+++ src/main/java/com/patrick/aviation/model/Gate.java
@@ -1,6 +1,5 @@
 package com.patrick.aviation.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -26,6 +25,5 @@ public class Gate {
 
     @ManyToOne
     @JoinColumn(name = "airport_id", nullable = false)
-    @JsonIgnore
     private Airport airport;
 }

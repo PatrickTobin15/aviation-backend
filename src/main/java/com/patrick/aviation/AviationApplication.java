a84ff7b5755eec2bac3a4f9399ad92857082eaff
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 02:25:41 -0230

    Update AviationApplication.java

--- src/main/java/com/patrick/aviation/AviationApplication.java
+++ src/main/java/com/patrick/aviation/AviationApplication.java
@@ -1,1 +1,12 @@
+package com.patrick.aviation;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AviationApplication.class, args);
+    }
+}

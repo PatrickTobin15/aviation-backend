6d53576c1a7b1ead7116f506699d02ffe6abfcf1
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 02:22:03 -0230

    Update AirportRepository.java

--- src/main/java/com/patrick/aviation/repository/AirportRepository.java
+++ src/main/java/com/patrick/aviation/repository/AirportRepository.java
@@ -1,1 +1,8 @@
+package com.patrick.aviation.repository;
 
+import com.patrick.aviation.model.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AirportRepository extends JpaRepository<Airport, Long> {
+    Airport findByCode(String code);
+}

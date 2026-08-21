2a40cba122594bc9c0f081b8a25f71ca7a1e74b6
PatrickTobin15 <patrick.tobin@keyin.com>
2026-08-21 02:22:35 -0230

    Update FlightRepository.java

--- src/main/java/com/patrick/aviation/repository/FlightRepository.java
+++ src/main/java/com/patrick/aviation/repository/FlightRepository.java
@@ -1,1 +1,15 @@
+package com.patrick.aviation.repository;
 
+import com.patrick.aviation.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+    // Departures for an airport
+    List<Flight> findByOriginAirportId(Long airportId);
+
+    // Arrivals for an airport
+    List<Flight> findByDestinationAirportId(Long airportId);
+}
